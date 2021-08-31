7e7be9058874e57e2d8adfac2a03ed04f4a2290f
mohammad nasr <nasrmohammad4804@gmail.com>
2021-08-31 13:14:50 +0430

    for calling app witch decision go  to teacherPanel or StudentPanel

---
+++ project/src/main/java/service/program/Main.java
@@ -0,0 +1,12 @@
+package service.program;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+
+        app.initialize();
+        app.chooseWorkDirectory();
+
+    }
+}
